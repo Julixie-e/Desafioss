b13e5878e8a4fb8dae073c83b7dba540cc6970e2
Julixie-e <xiejuli01gmail.com>
2024-06-06 22:07:04 -0300

    desafio7

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+const producto = document.querySelector ('#producto');
+
+producto.style.color = 'red';
+producto.style.fontSize = '3em';
+
+document.getElementById(precio).innerText = '$450';
+
+
+
+
