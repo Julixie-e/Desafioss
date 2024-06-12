0be529c939621921a9fd3628161e64edfc8dff53
Julixie-e <xiejuli01gmail.com>
2024-06-12 00:43:56 -0300

    desafio 9

--- js/main.js
+++ js/main.js
@@ -5,9 +5,41 @@ titulo.style.fontSize = "24px";
 
 const productos = [ "Camisa", "Zapatillas", "Bufanda", "Gorra", "Pantalón" ];
 
-for (const producto of productos) {    
+for (const producto of productos) {  
+    
+const contenedorProducto = document.createElement ("div")
 const h2 = document.createElement ("h2")
+
 h2.innerHTML = producto;
 document.body.appendChild (h2)
+
+
+
+let boton = document.createElement("button");
+
+boton.innerHTML = "Agregar al carrito";
+
+boton.id = "boton-" + producto.toLowerCase().replace(" ", "-");
+
+
+boton.addEventListener ("click",function() {
+
+    boton.innerHTML = "agregado";
+    
+    let mensaje = document.createElement ( "h3")
+    mensaje.textContent = " Agregado";
+
+    contenedorProducto.appendChild (mensaje)
+
 }
+
+)
+contenedorProducto.appendChild (boton);
+
+document.body.appendChild(contenedorProducto)
+
+}
+
+
+
 
